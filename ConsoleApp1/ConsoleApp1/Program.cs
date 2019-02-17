61c1a005790e48f7b0869c3d75a1f2e340c53e33
Kapustin Anton <kapustin.anton199712@gmail.com>
2019-02-17 19:14:21 +0700

    Podchet %7=0

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i1=1, i2=0, i3=0, i4=0, i5=0, i6=0, z;
+            Console.WriteLine("Список удачных билетов (сумма его цифр делится на 7):");
+          
+            while(i1<10)
+            {
+                while (i2 < 10)
+                {
+                    while (i3 < 10)
+                    {
+                        while (i4 < 10)
+                        {
+                            while (i5 < 10)
+                            {
+                                while (i6 < 10)
+                                {
+                                    z = i1 + i2 + i3 + i4 + i5 + i6;
+                                    
+                                    if (z % 7 == 0)
+                                        Console.WriteLine(i1+""+ i2 +""+ i3+"" + i4 +""+ i5 +""+ i6);
+                                    i6++;
+                                }
+                                i6 = 0;
+                                i5++;
+                            }
+                            i5 = 0;
+                            i4++;
+                        }
+                        i4 = 0;
+                        i3++;
+                    }
+                    i3 = 0;
+                    i2++;
+                }
+                i2 = 0;
+                i1++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
